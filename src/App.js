d2c8732a7d7a6959a41522736acfc9849b76c173
Ajay kumar P V <ajaykumar1997.ak@gmail.com>
2022-10-21 18:03:45 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './index';
+import { Suspense, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei';
+
+function Model(props) {
+  const group = useRef();
+  const bakedTexture = useTexture('/baked2.jpg');
+  const { nodes, materials } = useGLTF('/uazz.glb');
+
+  // useFrame(({ clock }) => {
+  //   group.current.rotation.y = clock.getElapsedTime() * 0.2;
+  // });
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.baked.geometry}
+        material={nodes.baked.material}
+        position={[1.33, -0.04, -1.19]}
+        rotation={[0.03, 0.33, -0.01]}
+      >
+        <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+      </mesh>
+      <mesh position={[0.3, -0.25, 0]}>
+        <boxBufferGeometry args={[10, 0.5, 10]} />
+        <meshBasicMaterial color={'#9a6a5c'} />
+      </mesh>
+    </group>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{ fov: 45, position: [10, 4, 6] }}>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
